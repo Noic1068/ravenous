eae1c896653c6d978f5daa896ca1f9dff92d63c4
Adam Romanow Hede Jensen <adam@MacBook-Pro-tilhrende-Adam.local>
2025-06-25 13:49:44 +0200

    3.0 - with more correct filestructure for css

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Business from './Business.jsx'
-import BusinessList from './BusinessList.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BusinessList />
+    ¨<App />
   </StrictMode>
 )
